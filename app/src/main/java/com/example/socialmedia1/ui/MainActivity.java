e005c471ec21145b1eb2f661e7bf4db4c50cf174
danascape <danascape@gmail.com>
2023-06-30 00:44:54 +0530

    SM: Add broken but working dialog fragment
    Signed-off-by: danascape <danascape@gmail.com>

--- app/src/main/java/com/example/socialmedia1/ui/MainActivity.java
+++ app/src/main/java/com/example/socialmedia1/ui/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.socialmedia1.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.socialmedia1.PostDialogFragment;
 import com.example.socialmedia1.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        showEditDialog();
+                    }
+                }
+        );
+    }
+
+    private void showEditDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        PostDialogFragment editNameDialogFragment = PostDialogFragment.newInstance("Some Title");
+        editNameDialogFragment.show(fm, "fragment_edit_name");
     }
 }
