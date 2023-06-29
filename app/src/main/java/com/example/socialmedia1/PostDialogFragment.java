547bbbede5fd0e733a4bd6888a83bc62399c66a5
Debobroto Das <debojitdas0707@gmail.com>
2023-06-30 01:41:32 +0530

    added dialogfragment design xml

--- app/src/main/java/com/example/socialmedia1/PostDialogFragment.java
+++ app/src/main/java/com/example/socialmedia1/PostDialogFragment.java
@@ -1,5 +1,7 @@
 package com.example.socialmedia1;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +26,7 @@ public class PostDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         return inflater.inflate(R.layout.fragment_dialog, container);
     }
 
