2f9282c6797a448102f55c2b7a80de65bdf2b047
Debobroto Das <debojitdas0707@gmail.com>
2023-07-02 13:40:02 +0530

    changed the view of activity_signup.xml, optimized PostDialogFragment.java code

--- app/src/main/java/com/example/socialmedia1/ui/activities/Signup.java
+++ app/src/main/java/com/example/socialmedia1/ui/activities/Signup.java
@@ -17,93 +17,90 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class Signup extends AppCompatActivity {
 
-    ImageView login,signup;
-    EditText email,password,confirmpassword, username;
-    FirebaseAuth mAuth;
+    private ImageView login, signup;
+    private EditText email, password, confirmPassword, username;
+    private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference;
 
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://socialmedia1-310bc-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("users");
+
         login = findViewById(R.id.login);
         signup = findViewById(R.id.signup);
         email = findViewById(R.id.emailtxt);
         password = findViewById(R.id.passwordtxt);
+        confirmPassword = findViewById(R.id.confirmPassword);
         username = findViewById(R.id.username);
-        mAuth = FirebaseAuth.getInstance();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Signup.this, Login.class);
                 startActivity(intent);
+                finish();
             }
         });
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String emailtxt = email.getText().toString().trim();
-                String passwordtxt = password.getText().toString().trim();
+                String emailTxt = email.getText().toString().trim();
+                String passwordTxt = password.getText().toString().trim();
+                String confirmPasswordTxt = confirmPassword.getText().toString().trim();
+                String usernameTxt = username.getText().toString().trim();
 
-                if (TextUtils.isEmpty(emailtxt)) {
+                if (TextUtils.isEmpty(emailTxt)) {
                     Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                if (TextUtils.isEmpty(emailtxt)) {
+                if (TextUtils.isEmpty(passwordTxt)) {
                     Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                mAuth.createUserWithEmailAndPassword(emailtxt, passwordtxt)
+
+                if (!passwordTxt.equals(confirmPasswordTxt)) {
+                    Toast.makeText(getApplicationContext(), "Passwords do not match!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(emailTxt, passwordTxt)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     String userId = mAuth.getCurrentUser().getUid();
-                                    String usernametxt = username.getText().toString();
-                                    String passwordtxt = password.getText().toString();
-                                    String emailtxt = email.getText().toString();
+
+                                    // Save user details to the database
+                                    databaseReference.child(userId).child("username").setValue(usernameTxt);
+                                    databaseReference.child(userId).child("email").setValue(emailTxt);
+                                    databaseReference.child(userId).child("password").setValue(passwordTxt);
+
                                     Toast.makeText(Signup.this, "Account Created.", Toast.LENGTH_SHORT).show();
-                                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-                                        @Override
-                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                            databaseReference.child("users").child(userId).child("username").setValue(usernametxt);
-                                            databaseReference.child("users").child(userId).child("email").setValue(emailtxt);
-                                            databaseReference.child("users").child(userId).child("password").setValue(passwordtxt);
-                                        }
-
-                                        @Override
-                                        public void onCancelled(@NonNull DatabaseError error) {
-
-                                        }
-                                    });
-                                    // You can perform any additional actions here, such as navigating to another activity.
+                                    startActivity(new Intent(Signup.this, Login.class));
+                                    finish();
                                 } else {
                                     Exception exception = task.getException();
                                     if (exception != null) {
                                         String errorMessage = exception.getMessage();
                                         Log.e("SignupActivity", "Signup failed", exception);
-                                        Toast.makeText(getApplicationContext(), "Signup failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getApplicationContext(), "Signup failed: " + errorMessage, Toast.LENGTH_SHORT).show();
                                     }
                                 }
                             }
-
                         });
             }
         });
-
-
-
     }
 }
--- app/src/main/java/com/example/socialmedia1/ui/fragments/PostDialogFragment.java
+++ app/src/main/java/com/example/socialmedia1/ui/fragments/PostDialogFragment.java
@@ -16,17 +16,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.example.socialmedia1.R;
 import com.example.socialmedia1.utils.VibrationUtils;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
-import com.example.socialmedia1.R;
 
 public class PostDialogFragment extends DialogFragment {
     private EditText mEditText;
@@ -38,25 +36,25 @@ public class PostDialogFragment extends DialogFragment {
         frag.setArguments(args);
         return frag;
     }
+
     private FirebaseFirestore firestore;
     private CollectionReference collectionRef;
     private EditText posttxt;
     private ImageView addpost, addimage;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         firestore = FirebaseFirestore.getInstance();
         String collectionPath = "dsiblr";
         collectionRef = firestore.collection(collectionPath);
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         View view = inflater.inflate(R.layout.fragment_dialog, container, false);
-        Map<String, Object> data = new HashMap<>();
+
         posttxt = view.findViewById(R.id.etPost);
         addpost = view.findViewById(R.id.postbtn);
         addimage = view.findViewById(R.id.addimagebtn);
@@ -64,59 +62,51 @@ public class PostDialogFragment extends DialogFragment {
         addimage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(view.getContext(), "Image feature yet to added.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(view.getContext(), "Image feature yet to be added.", Toast.LENGTH_SHORT).show();
             }
         });
+
         addpost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(posttxt.getText().toString().isEmpty()){
+                String postText = posttxt.getText().toString().trim();
+                if (postText.isEmpty()) {
                     Toast.makeText(view.getContext(), "Empty Post", Toast.LENGTH_SHORT).show();
                     VibrationUtils.vibrate(getContext(), 200);
-                }
-                else{
-                    data.put("post text", posttxt.getText().toString());
+                } else {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("post text", postText);
                     data.put("time", com.google.firebase.Timestamp.now());
-                    createDocument(data,view.getContext());
+                    createDocument(data, view.getContext());
                     dismiss();
                 }
-
             }
         });
+
         return view;
     }
 
     private void createDocument(Map<String, Object> data, Context context) {
-        collectionRef.add(data).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentReference> task) {
-                if (task.isSuccessful()) {
-                    DocumentReference documentRef = task.getResult();
-                    if (documentRef != null) {
-                        // Document created successfully
-                        String documentId = documentRef.getId();
-                        Toast.makeText(context, "Posted!", Toast.LENGTH_SHORT).show();
+        collectionRef.add(data)
+                .addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(context, "Posted!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(context, "Could not post!", Toast.LENGTH_SHORT).show();
+                        }
                     }
-                } else {
-                    // Error creating document
-                    Toast.makeText(context, "Not posted!", Toast.LENGTH_SHORT).show();
-                }
-            }
-            });
-        }
-
+                });
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Get field from view
         mEditText = (EditText) view.findViewById(R.id.etPost);
-        // Fetch arguments from bundle and set title
         String title = getArguments().getString("title", "Enter Name");
         getDialog().setTitle(title);
-        // Show soft keyboard automatically and request focus to field
         mEditText.requestFocus();
-        getDialog().getWindow().setSoftInputMode(
-                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 }
