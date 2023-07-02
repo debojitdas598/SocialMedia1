312c1960fd92da37ac2ec240224f27db13b80a6b
Debobroto Das <debojitdas0707@gmail.com>
2023-07-02 20:49:17 +0530

    added liked and reply button (not functioning yet)

--- app/src/main/java/com/example/socialmedia1/ui/activities/BoardSelector.java
+++ app/src/main/java/com/example/socialmedia1/ui/activities/BoardSelector.java
@@ -1,16 +1,34 @@
 package com.example.socialmedia1.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.socialmedia1.R;
+import com.google.android.material.card.MaterialCardView;
 
 public class BoardSelector extends AppCompatActivity {
-
+    MaterialCardView dsicardview,bmscecardview;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_selector);
+        dsicardview = findViewById(R.id.dsiblrcard);
+        bmscecardview = findViewById(R.id.bmscecard);
+
+        dsicardview.setOnClickListener(v -> {
+            intent = new Intent(getApplicationContext(),MainActivity.class);
+            intent.putExtra("Collection Key","dsiblr");
+            startActivity(intent);
+        });
+        bmscecardview.setOnClickListener(v -> {
+            Toast.makeText(this, "Under Development!", Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
--- app/src/main/java/com/example/socialmedia1/ui/fragments/PostDialogFragment.java
+++ app/src/main/java/com/example/socialmedia1/ui/fragments/PostDialogFragment.java
@@ -11,20 +11,15 @@ import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-
 import com.example.socialmedia1.R;
 import com.example.socialmedia1.utils.VibrationUtils;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -53,16 +48,13 @@ public class PostDialogFragment extends DialogFragment {
         String collectionPath = "dsiblr";
         collectionRef = firestore.collection(collectionPath);
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        View view = inflater.inflate(R.layout.fragment_dialog, container, false);
-
+    private void initializeViews(View view){
         posttxt = view.findViewById(R.id.etPost);
         addpost = view.findViewById(R.id.postbtn);
         addimage = view.findViewById(R.id.addimagebtn);
+    }
 
+    private void onClickfunctions(View view){
         addimage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,16 +74,21 @@ public class PostDialogFragment extends DialogFragment {
                     data.put("post text", postText);
                     data.put("time", com.google.firebase.Timestamp.now());
                     createDocument(data, view.getContext());
-
                 }
             }
         });
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        View view = inflater.inflate(R.layout.fragment_dialog, container, false);
+        initializeViews(view);
+        onClickfunctions(view);
         return view;
     }
 
     private void createDocument(Map<String, Object> data, Context context) {
-
        firestore.collection("dsiblr").document(docName())
                .set(data)
                .addOnSuccessListener(new OnSuccessListener<Void>() {
